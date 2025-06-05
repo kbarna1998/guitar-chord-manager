bc26159c10a8312e2314f72097c4f43fb15133c1
kbarna1998 <k.barna1998@gmail.com>
2025-06-05 09:17:59 +0200

    Add javafx

---
+++ src/test/java/org/example/model/ChordTest.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChordTest {
+
+    private Chord underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Chord();
+    }
+
+}
